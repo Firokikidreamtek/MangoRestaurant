587cc83ba117eb6223a4dece58472b874d4fd107
Firokikidreamtek <Firokikidreamtek@yandex.ru>
2022-10-11 18:41:23 +0300

    Base Service Interface and API Request Model

--- Mango.Web/Models/ApiRequest.cs
+++ Mango.Web/Models/ApiRequest.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Mango.Web.SD;
 
 namespace Mango.Web.Models
 {
     public class ApiRequest
     {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string ApiUrl { get; set; }
+        public object Data { get; set; }
+        public string AccessToken { get; set; }
 
     }
 }
---
+++ Mango.Web/Services/BaseService.cs
@@ -0,0 +1,26 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Models.Dto;
+using Mango.Web.Services.IServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services
+{
+    public class BaseService : IBaseService
+    {
+        ResponseDto IBaseService.responseModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        void IDisposable.Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<T> IBaseService.SendAsync<T>(ApiRequest apiRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
