23cd1625d68b97c8fbb97d966f123772ab4f9a38
Firokikidreamtek <Firokikidreamtek@yandex.ru>
2022-10-11 17:49:51 +0300

    Add migration with images for product from Azure Storage in ProductAPIService
    Start working with files in folder Mango.Web

--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using Mango.Web.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Web.Controllers
 {
@@ -21,12 +19,6 @@ namespace Mango.Web.Controllers
         public IActionResult Privacy()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
---
+++ Mango.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,19 @@
+﻿using Mango.Web.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mango.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<T> GetAllAsync<T>();
+        Task<T> GetByIdAsync<T>(int id);
+        Task<T> CreateProductAsync<T>(ProductDto product);
+        Task<T> UpdateProductAsync<T>(ProductDto product);
+        Task<T> DeleteProductAsync<T>(int id);
+
+    }
+}
